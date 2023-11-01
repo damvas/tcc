e1862a02dbfeb5e7229e5d2d2391da80fbe5d0af
damvas <123704398+damvas@users.noreply.github.com>
2023-10-31 23:12:08 -0300

    Update mun_coleta.py

--- mun_coleta.py
+++ mun_coleta.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def get_data():
     df = pd.read_csv(r'C:\Users\danie\Desktop\TCC\Dados\ADH\mun_2010.csv', sep = ';', encoding = 'latin1', decimal = ',')
+    df['pmpob'] = df['pmpob']/100
 
     pib = get_pib()
     df['id'] = df['id'].astype(str)
@@ -11,21 +12,23 @@ def get_data():
     eci = get_eci()
     df = pd.merge(df, eci, 'left', 'id')
     df = df.drop(columns = 'mun')
-    df = df.dropna()
 
     cambio = get_exchange_rate()
     exp = get_exp()
     exp = convert_exp(exp, cambio)
-    df = pd.merge(df,exp,'left',['municipio','sg_uf']).dropna()
+    df = pd.merge(df,exp,'left',['municipio','sg_uf'])
 
     imp = get_imp()
     imp = convert_imp(imp, cambio)
-    df = pd.merge(df,imp,'left',['municipio','sg_uf']).dropna()
+    df = pd.merge(df,imp,'left',['municipio','sg_uf'])
 
     df = df.reset_index(drop=True)
+    df = df.rename(columns = {'exp_brl': 'exp', 'imp_brl': 'imp', 'pib_brl': 'pib', 'pesotot': 'pop'})
+    df['exp'] = df['exp'].fillna(0)
+    df['imp'] = df['imp'].fillna(0)
+    df = df.query("eci.notna()").reset_index(drop=True)
 
     df = df[['sg_uf','uf','municipio','id'] + [col for col in df.columns if col not in ['id','uf','sg_uf','municipio']]]
-    df = df.rename(columns = {'exp_brl': 'exp', 'imp_brl': 'imp', 'pib_brl': 'pib', 'pesotot': 'pop'})
     df.to_csv(r'C:\Users\danie\Desktop\TCC\Dados\BASE\base_mun_raw.csv', index = False, sep = ';', decimal = ',', encoding = 'latin1')
 
 def download_adh_mun_2010():
